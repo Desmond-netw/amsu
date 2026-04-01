632f4816793b3ee7b5e0316e91abfe8d0f86eb7d
Desmond-netw <dsquare200@gmail.com>
2026-04-01 15:12:14 +0300

    home page project card, projectmini secction and thumb images uplodated

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HomeBanner from "./ui_components/HomeBanner/HomeBanner";
 import { About } from "./ui_components/about/About";
 import Stats from "./ui_components/statics/statics";
 import Mandate from "./ui_components/mandate/Mandate";
+import ProjectMini from "./ui_components/Projectmini/project";
 export default function Home() {
   return (
     <div>
@@ -9,6 +10,7 @@ export default function Home() {
       <About />
       <Stats />
       <Mandate />
+      <ProjectMini />
     </div>
   );
 }
---
+++ app/ui_components/Projectmini/project.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Projectcard from "../Card/ProjectCard";
+import Pretitle from "../pretitle/Pretitile";
+
+const projectData = [
+  {
+    id: 1,
+    title: "Waste Stabilization Pond",
+    description:
+      "Waste Stabilization Ponds (WSP) have proven to be effective for treating wastewater, and the construction of low energy-consuming ecosystems that use natural processes...",
+    imageUrl: "/assets/projectmini/thumb1.jpg",
+  },
+  {
+    id: 2,
+    title: "Laboratory and Testing Services",
+    description:
+      "Physicochemical parameters such as pH, temperature, BOD, COD, TSS, DO, nutrients, and more are monitored to assess wastewater quality...",
+    imageUrl: "/assets/projectmini/thumb1.jpg",
+  },
+  {
+    id: 3,
+    title: "Sewerage machinery and vehicles",
+    description:
+      "Our range of sewerage vehicles and machines are essential for the efficient operation and maintenance of wastewater collection systems...",
+    imageUrl: "/assets/projectmini/thumb3.jpg",
+  },
+];
+
+const ProjectMini = () => {
+  return (
+    <section className="py-20 px-6 xl:px-24 bg-gradient-to-b from-[#bfe9ff] via-[#e8f7ff] to-white">
+      <div className="max-w-6xl mx-auto">
+        {/* Header */}
+        <div className="text-center mb-12">
+          <Pretitle text="Check out" center={true} />
+          <h2 className="h2 mt-4 text-primary text-center">
+            Projects & Innovations
+          </h2>
+        </div>
+
+        {/* Cards Grid */}
+        <div className="grid gap-12 md:grid-cols-2 lg:grid-cols-3">
+          {projectData.map((data) => (
+            <Projectcard
+              key={data.id}
+              title={data.title}
+              description={data.description}
+              imageUrl={data.imageUrl}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProjectMini;

d8e24d752113cdebb5fa33ea505c3b17be087da0
Desmond-netw <dsquare200@gmail.com>
2026-04-05 16:48:54 +0300

    modify Mobile navbar background

--- app/ui_components/Mobile/MobileNav.tsx
+++ app/ui_components/Mobile/MobileNav.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Link from "next/link"; // Changed from react-scroll
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { RiMenu3Fill } from "react-icons/ri";
 import {
@@ -29,30 +29,55 @@ const MobileNav = () => {
 
   return (
     <Sheet open={isOpen} onOpenChange={setIsOpen}>
-      <SheetTrigger className="text-white flex items-center justify-center text-3xl">
+      {/* Toggle Button */}
+      <SheetTrigger
+        className="
+          text-brand_1-500 
+          hover:text-brand_1-600 
+          active:text-brand_1-700 
+          flex items-center justify-center 
+          text-3xl transition-colors
+        "
+      >
         <RiMenu3Fill />
       </SheetTrigger>
 
-      <SheetContent side="right" className="bg-primary text-white border-none">
+      {/* Mobile Menu */}
+      <SheetContent
+        side="right"
+        className="
+          bg-brand_1-500 
+          text-white 
+          border-none 
+          px-6
+        "
+      >
         <div className="flex flex-col pt-16 pb-8 items-center justify-between h-full">
           <SheetHeader>
             <SheetTitle>
-              <Logo />
+              <Logo width={100} height={50} />
             </SheetTitle>
             <SheetDescription className="sr-only">
               Navigation Menu
             </SheetDescription>
           </SheetHeader>
 
+          {/* Nav Links */}
           <ul className="w-full flex flex-col gap-6 items-center justify-center">
             {links.map((link, indx) => (
               <li key={indx}>
                 <Link
                   href={link.path}
-                  onClick={() => setIsOpen(false)} // Closes sheet on click
-                  className={`text-white uppercase font-semibold tracking-[1.2px] text-lg hover:text-brand-yellow transition ${
-                    pathname === link.path ? "text-brand-yellow" : ""
-                  }`}
+                  onClick={() => setIsOpen(false)}
+                  className={`
+                    uppercase font-semibold tracking-[1.2px] text-lg 
+                    transition-colors
+                    ${
+                      pathname === link.path
+                        ? "text-brand-yellow"
+                        : "text-white hover:text-brand-yellow"
+                    }
+                  `}
                 >
                   {link.name}
                 </Link>
@@ -60,6 +85,7 @@ const MobileNav = () => {
             ))}
           </ul>
 
+          {/* Social Icons */}
           <div className="mt-10">
             <Socials containerStyles="text-white text-xl flex gap-6" />
           </div>

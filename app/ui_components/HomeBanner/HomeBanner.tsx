2b8a9ef21ae1b19ec44d0dfa6d49500805ae9ace
Desmond-netw <dsquare200@gmail.com>
2026-04-12 23:21:50 +0300

    modify homebanner

--- app/ui_components/HomeBanner/HomeBanner.tsx
+++ app/ui_components/HomeBanner/HomeBanner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomeBanner = () => {
   return (
-    <section className="relative h-[300px] bg-[url('/assets/banner/banner.jpg')] bg-cover bg-center bg-no-repeat">
+    <section className="relative h-[300px] bg-[url('/assets/banner/heroBanner.webp')] bg-cover bg-center bg-no-repeat">
       <div className="absolute inset-0 bg-brand_1-900 bg-opacity-50"></div>
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4">
         <h5 className="text-2xl md:text-3xl font-bold mb-4 uppercase">

d098997661b91f69a7ad9facb2aaf0b017b4d305
Desmond-netw <dsquare200@gmail.com>
2026-04-23 20:52:26 +0300

    modify serviceCard

--- app/ui_components/Card/serviceCard.tsx
+++ app/ui_components/Card/serviceCard.tsx
@@ -27,7 +27,7 @@ const ServiceCard = ({ title, bgImage }: ServiceCardProps) => {
       />
 
       {/* Brand Overlay */}
-      <div className="absolute inset-0 bg-brand_1-500/70 group-hover:bg-brand_1-500/60 transition-all duration-500" />
+      <div className="absolute inset-0 bg-slate-800/50 group-hover:bg-slate-900/30 transition-all duration-500" />
 
       {/* Title */}
       <div className="absolute inset-0 flex items-center justify-center px-4">

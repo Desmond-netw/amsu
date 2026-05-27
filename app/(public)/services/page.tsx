93619987139e21a5e7a12319e94f2aad59264253
Desmond-netw <dsquare200@gmail.com>
2026-05-27 08:15:15 +0300

    img wastetreatment fix on service

--- app/(public)/services/page.tsx
+++ app/(public)/services/page.tsx
@@ -8,7 +8,7 @@ import Pretitle from "../ui_components/pretitle/Pretitile";
 const servicesData = [
   {
     title: "Sewerage Collection and Treatment",
-    bgImage: "/assets/services/treatment/banner.webp",
+    bgImage: "/assets/services/waste_treatment/banner.webp",
     link: "/services/treatment",
   },
   {
